27340fd6380acb2a2ba521bc57ba59fe44006fba
hyejeongjang <hj970219@naver.com>
2020-08-10 07:39:13 +0900

    change dirpath

--- server.js
+++ server.js
@@ -1,36 +1,21 @@
-const http = require("http");
-const url = require("url");
-const fs = require("fs");
-http
-  .createServer((request, response) => {
-    console.log("server.start!");
-    const path = url.parse(request.url, true).pathname; // url에서 path 추출
-    if (request.method === "GET") {
-      // GET 요청이면
-      if (path === "/about") {
-        // 주소가 /about이면
-        response.writeHead(200, { "Content-Type": "text/html" }); // header 설정
-        fs.readFile(__dirname + "/about.html", (err, data) => {
-          // 파일 읽는 메소드
-          if (err) {
-            return console.error(err); // 에러 발생시 에러 기록하고 종료
-          }
-          response.end(data, "utf-8"); // 브라우저로 전송
-        });
-      } else if (path === "/video") {
-        // 주소가 /이면
-        response.writeHead(200, { "Content-Type": "text/html" });
-        fs.readFile(__dirname + "/video-conferencing.html", (err, data) => {
-          if (err) {
-            return console.error(err);
-          }
-          response.end(data, "utf-8");
-        });
-      } else {
-        // 매칭되는 주소가 없으면
-        response.statusCode = 404; // 404 상태 코드
-        response.end("주소가 없습니다");
-      }
-    }
-  })
-  .listen(8080);
+const express = require("express");
+const path = require("path");
+const app = express();
+const fs = require("fs"); // 파일 로드
+
+app.use(express.static(__dirname + "/src"));
+//app.use(express.static(path.join(__dirname, "html")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "html", "video-conferencing.html"));
+});
+app.get("/about", (req, res) => {
+  //res.sendFile(path.join(__dirname, "html", "about.html"));
+  fs.readFile("/html/video-conferencing.html", function (error, data) {
+    res.writeHead(200, { "Content-Type": "test/html" });
+    res.end(data);
+  });
+});
+app.listen(8080, () => {
+  console.log("Express App on port 8080!");
+});
